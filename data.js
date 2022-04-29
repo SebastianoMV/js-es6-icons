4ffdc00c9daa6e143a35fd34f9d12c8443f7ed8d
SebastianoMV <sebastianovisco@gmail.com>
2022-04-29 17:31:23 +0200

    Milestone 3 completata in qualche modo

--- data.js
+++ data.js
@@ -113,18 +113,54 @@ const icone = [
 	}
 ];
 
+const iconFiltred = [];
 
-function genera(i){
+document.getElementById('from-select').addEventListener('change', function(){
+	iconFiltred.length = 0;
+	let valueSelected = this.value;
+	generaFiltred(valueSelected);
+	console.log(this.value);
+	document.querySelector('.row').innerHTML = '';
+	if(valueSelected !=null){
+		for(let i in iconFiltred){
+			genera(i,iconFiltred);
+		}
+	}
+	if(valueSelected == 'all'){
+		for(let i in icone){
+			genera(i,icone);
+		}
+	}
+
+
+});
+
+
+function genera(i, nomeArray){
 	const output = `
 	<div class="col col-2 mt-2 mb-5 mx-2">
-    <div class="card d-flex align-items-center justify-content-center" style="width: 100%; height: 100px;border-radius: 10px;  color: ${icone[i].color}; font-size: 50px;">
-      <i class="${icone[i].family} ${icone[i].prefix}${icone[i].name}"></i>
+    <div class="card d-flex align-items-center justify-content-center" style="width: 100%; height: 100px;border-radius: 10px;  color: ${nomeArray[i].color}; font-size: 50px;">
+      <i class="${nomeArray[i].family} ${nomeArray[i].prefix}${nomeArray[i].name}"></i>
       <h5 class="card-title" style="color: black;">${icone[i].name}</h5>
     </div>
   </div>`
 	document.querySelector('.row').innerHTML += output;
 }
 
+
 for(let i in icone){
-  genera(i);
+	genera(i,icone);
 }
+
+
+
+function generaFiltred (value){
+  for (let i = 0; i < icone.length; i++) {
+		if(icone[i].type == value){ 
+			iconFiltred.push(icone[i]);
+		 } 	
+	}
+  
+} 
+
+
